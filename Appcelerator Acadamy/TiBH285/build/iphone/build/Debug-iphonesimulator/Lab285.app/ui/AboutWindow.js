99ee5052770c6b22059210a6f90926642518bd2f
hyokos <hyokos@me.com>
2012-12-15 05:16:56 -0500

    Facebook Component

--- Appcelerator Acadamy/TiBH285/build/iphone/build/Debug-iphonesimulator/Lab285.app/ui/AboutWindow.js
+++ Appcelerator Acadamy/TiBH285/build/iphone/build/Debug-iphonesimulator/Lab285.app/ui/AboutWindow.js
@@ -1,16 +1,1 @@
-var AboutWindow = function() {
-	// create the About window
-	var aboutWindow = Titanium.UI.createWindow({  
-	    title:L('about'),
-	    barColor:'#18223c'
-	});
-	// create the webview, reading in the about.html file
-	// we could have used the html property instead, and provided inline HTML content
-	var aboutWebView = Titanium.UI.createWebView({  
-		url: '/about.html'
-	});
-	aboutWindow.add(aboutWebView);
-	return aboutWindow;
-}; // end createAboutWindow();
-
-module.exports = AboutWindow;
+/Users/hyokosdeveloper/git/ZeusbaseTi/Appcelerator Acadamy/TiBH285/Resources/ui/AboutWindow.js
